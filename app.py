3415588dae3083fce482ad3468ceab65746aa8a5
eswaribala <Parameswaribala@gmail.com>
2021-04-21 16:07:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+# app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def blog():
+    return "Flask in kind Kubernetes cluster"
+
+if __name__ == '__main__':
+    app.run(threaded=True,host='0.0.0.0',port=8087)
